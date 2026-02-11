2c2fdf51952768e253a912cbf1061b10bb58e9c1
Aymen Ourdjini <aymen.esi.2016@gmail.com>
2026-02-11 10:48:09 +0100

    feat: add Country model with indexes, admin registration and initial migrations

--- config/countries/models.py
+++ config/countries/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+   # Identifiants
+    cca3 = models.CharField(max_length=3, unique=True, primary_key=True)
+    cca2 = models.CharField(max_length=2, db_index=True)
+
+    # Noms
+    common_name = models.CharField(max_length=255)
+    official_name = models.CharField(max_length=255)
+
+    # Localisation
+    capital = models.CharField(max_length=255, blank=True, null=True)
+    region = models.CharField(max_length=100, db_index=True)
+    subregion = models.CharField(max_length=100, blank=True, null=True)
+
+    # Données numériques
+    population = models.BigIntegerField(db_index=True)
+    area = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
+
+    # Autres 
+    flag_url = models.URLField(max_length=500, blank=True, null=True)
+    currencies = models.JSONField(default=dict, blank=True)
+
+    # Métadonnées
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['common_name']
+        verbose_name_plural = "Countries"
+
+    def __str__(self):
+        return f"{self.common_name} ({self.cca3})"
+
